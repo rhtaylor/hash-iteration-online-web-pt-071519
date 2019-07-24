29d9b7c0a593569295fef847fe6aa939ebb65bfe
Randy Taylor <randyisbusy@gmail.com>
2019-07-24 20:19:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,7 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |key, val|
     if val < 12
       puts "Happy Birthday #{key}! You are now #{val} years old!"
+    else puts "You are too old for this."
     end
   end
 end
