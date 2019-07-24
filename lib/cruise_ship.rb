2ebe953c910b9ba5643db3a3c0f5f07e2815c371
Randy Taylor <randyisbusy@gmail.com>
2019-07-24 19:27:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,13 +1,13 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
 def select_winner(passengers)
-  passengers.each do |key, val| 
+  passengers.each do |key, val|
     if key == suite_a: && val[0] == "a"
       return val
 end
