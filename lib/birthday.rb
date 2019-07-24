8055df7e6891f99a343aa7b4378f664b472047db
Randy Taylor <randyisbusy@gmail.com>
2019-07-24 20:08:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,12 +1,10 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each |key,val| 
+  birthday_kids.each |key,val|
     "Happy Birthday #{key}! You are now #{val} years old!!"
 end
-
-
